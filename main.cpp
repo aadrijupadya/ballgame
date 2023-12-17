dd3b859a3dfdf7b4c34762f3ff7727fcd6b04799
Aadrij Upadya <adrij2005@gmail.com>
2023-12-16 22:06:30 -0800

    Added some functions and movement, some sprites too

--- main.cpp
+++ main.cpp
@@ -5,32 +5,60 @@
 #include <SFML/Window.hpp>
 #include <SFML/Config.hpp>
 #include <SFML/Audio.hpp>
+#include <time.h>
 #include <SFML/OpenGL.hpp>
-
 #include <iostream>
 #include <cstdlib>
 #include <string>
 
 using namespace std;
 
+bool check_oob(sf::Sprite spr)
+{
+    if (spr.getPosition().x >= 750 || spr.getPosition().x <= 0)
+    {
+        return true;
+    }
+    return false;
+}
 void run_game()
 {
     string name1, name2;
     cout << "Enter Names" << endl;
     cin >> name1;
     cin >> name2;
-    sf::RenderWindow game_window(sf::VideoMode(800, 480), "Gameplay");
+    sf::RenderWindow game_window(sf::VideoMode(750, 480), "Gameplay");
     sf::Text player_1_text;
     sf::Text player_2_text;
     sf::Text score_1_text;
     int score_1 = 0;
     sf::Text score_2_text;
     int score_2 = 0;
+    sf::Texture player_1_texture;
+    sf::Texture hoop_texture;
+
+    if (!player_1_texture.loadFromFile("/Users/aadrijupadya/Downloads/player_new.png"))
+    {
+        cout << "Error loading texture" << endl;
+    }
+
+    if (!hoop_texture.loadFromFile("/Users/aadrijupadya/Downloads/hoop.png"))
+    {
+        cout << "Error loading texture" << endl;
+    }
+
+    sf::Sprite player_1_image;
+    sf::Sprite player_2_image;
+
+    sf::Sprite left_hoop;
+    sf::Sprite right_hoop;
 
     sf::Texture background;
     sf::Font font;
     sf::Keyboard keys;
     sf::Event event;
+    bool p1_left = false, p2_left = false, p1_right = false, p2_right = false;
+    ;
 
     if (!font.loadFromFile("/Users/aadrijupadya/Downloads/roboto/Roboto-Medium.ttf"))
     {
@@ -66,20 +94,133 @@ void run_game()
     player_2_text.setStyle(sf::Text::Bold);
     player_2_text.setPosition(600.f, 20.f);
 
+    player_1_image.setTexture(player_1_texture);
+    player_1_image.scale(0.2, 0.22f);
+    player_1_image.setPosition(300, 360);
+
+    player_2_image.setTexture(player_1_texture);
+    player_2_image.scale(-0.2f, 0.22f);
+    player_2_image.setPosition(450, 360);
+
+    left_hoop.setTexture(hoop_texture);
+    left_hoop.scale(0.2, 0.2);
+    left_hoop.setPosition(-50, 70);
+
+    right_hoop.setTexture(hoop_texture);
+    right_hoop.scale(-0.2, 0.2);
+    right_hoop.setPosition(800, 70);
+
     while (game_window.isOpen())
     {
+        // p1_left = false;
+        // p1_right = false;
+        // p2_left = false;
+        // p2_right = false;
         while (game_window.pollEvent(event))
         {
             // std::cout << "Here is the game" << std::endl;
 
             if (event.type == sf::Event::Closed)
+            {
                 game_window.close();
+            }
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Right)
+                {
+                    p2_right = true;
+                }
+
+                if (event.key.code == sf::Keyboard::Left)
+                {
+                    p2_left = true;
+                }
+
+                if (event.key.code == sf::Keyboard::A)
+                {
+                    p1_left = true;
+                }
+
+                if (event.key.code == sf::Keyboard::D)
+                {
+                    p1_right = true;
+                }
+            }
+            else if (event.type == sf::Event::KeyReleased)
+            {
+                if (event.key.code == sf::Keyboard::Left)
+                {
+                    p2_left = false;
+                }
+                else if (event.key.code == sf::Keyboard::Right)
+                {
+                    p2_right = false;
+                }
+
+                if (event.key.code == sf::Keyboard::A)
+                {
+                    p1_left = false;
+                }
+                else if (event.key.code == sf::Keyboard::D)
+                {
+                    p1_right = false;
+                }
+            }
         }
+        if (p1_right)
+        {
+            if (!(check_oob(player_1_image)))
+            {
+                player_1_image.move(0.05, 0);
+            }
+            else
+            {
+                player_1_image.setPosition(player_1_image.getPosition().x - 10, 360);
+            }
+        }
+        if (p1_left)
+        {
+            if (!(check_oob(player_1_image)))
+            {
+                player_1_image.move(-0.05, 0);
+            }
+            else
+            {
+                player_1_image.setPosition(player_1_image.getPosition().x + 10, 360);
+            }
+        }
+        if (p2_right)
+        {
+            if (!(check_oob(player_2_image)))
+            {
+                player_2_image.move(0.05, 0);
+            }
+            else
+            {
+                player_2_image.setPosition(player_2_image.getPosition().x - 10, 360);
+            }
+        }
+        if (p2_left)
+        {
+            if (!(check_oob(player_2_image)))
+            {
+                player_2_image.move(-0.05, 0);
+            }
+            else
+            {
+                player_2_image.setPosition(player_2_image.getPosition().x + 10, 360);
+            }
+        }
+
         game_window.clear();
         game_window.draw(player_1_text);
         game_window.draw(player_2_text);
         game_window.draw(score_1_text);
         game_window.draw(score_2_text);
+        game_window.draw(player_1_image);
+        game_window.draw(player_2_image);
+        game_window.draw(left_hoop);
+        game_window.draw(right_hoop);
         game_window.display();
     }
 }
